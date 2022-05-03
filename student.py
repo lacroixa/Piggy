fdd768f3d3ac26666e0646f4cae8996a83c2d0e6
Andre Lacroix <60482897+lacroixa@users.noreply.github.com>
2022-05-03 18:26:17 +0000

    d

--- student.py
+++ student.py
@@ -70,7 +70,7 @@ class Piggy(PiggyParent):
         # TODO: check to see if it's safe before dancing
         
         # lower-ordered example...
-        self.right(primary=50, counter=-50)
+        self.right(primary=100, counter=-100)
         time.sleep(2)
         self.stop()
         self.fwd()
