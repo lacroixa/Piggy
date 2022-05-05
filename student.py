b6734fa4befa6120d4a817aa217be198cbc41e82
Andre Lacroix <60482897+lacroixa@users.noreply.github.com>
2022-05-05 18:46:16 +0000

    std

--- student.py
+++ student.py
@@ -81,9 +81,7 @@ class Piggy(PiggyParent):
 
   
     def dance(self):
-        """A higher-ordered algorithm to make your robot dance"""
-        # TODO: check to see if it's safe before dancing
-      "self.safe_to_dance()
+      self.safe_to_dance()
       if self.safe_to_dance() == True
         self.right(primary=90,counter=-90)
         time.sleep(1)
