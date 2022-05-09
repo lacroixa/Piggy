5abb0aa73856343804bc6c7de270d1efdbadc3fa
Andre Lacroix <60482897+lacroixa@users.noreply.github.com>
2022-05-09 19:07:05 +0000

    b

--- student.py
+++ student.py
@@ -142,6 +142,9 @@ class Piggy(PiggyParent):
           time.sleep(1)
           self.stop()
           self.turn_by_deg(-45)
+          self.fwd()
+          time.sleep(2)
+          self.turn_by_deg(-45)
                   
 
     def scan(self):
