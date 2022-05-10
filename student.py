f29628e232070860754728fa0d82bc23cb6580c9
Andre Lacroix <60482897+lacroixa@users.noreply.github.com>
2022-05-10 18:29:24 +0000

    b

--- student.py
+++ student.py
@@ -125,6 +125,7 @@ class Piggy(PiggyParent):
           self.turn_by_deg(180)
 
     def move_around_box(self):
+      safe = True
       while True:
         self.servo(self.MIDPOINT)
         self.fwd()
@@ -143,9 +144,16 @@ class Piggy(PiggyParent):
           self.stop()
           self.turn_by_deg(-45)
           self.servo(2000)
-          if self.read_distance() <= 300:
-            self.turn_by_deg(45)
-          self.turn_by_deg(45)
+          while safe:
+            if self.read_distance() <= 300:
+              self.turn_by_deg(45)
+              self.fwd()
+              time.sleep(1)
+              self.stop
+              safe = True
+            else:
+              self.turn_by_deg(-45)
+              safe = False
 
             
             
