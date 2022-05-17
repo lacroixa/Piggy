83a47efee1d27e3dcbfc345e712fd1191a148040
Andre Lacroix <60482897+lacroixa@users.noreply.github.com>
2022-05-17 18:48:12 +0000

    ml

--- student.py
+++ student.py
@@ -207,7 +207,7 @@ class Piggy(PiggyParent):
         self.turn_by_deg(90)
         self.servo(2000)
         self.fwd
-        if self.read_distance > 650:
+        if self.read_distance() > 650:
           time.sleep(2)
           self.stop()
           self.turn_by_deg(-90)
@@ -215,7 +215,7 @@ class Piggy(PiggyParent):
         self.turn_by_deg(-90)
         self.servo(1000)
         self.fwd
-        if self.read_distance > 650:
+        if self.read_distance() > 650:
           time.sleep(2)
           self.stop()
           self.turn_by_deg(90)
