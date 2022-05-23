b3e50069a292576af04de9cc5eaeeb3b083d3ae7
Andre Lacroix <60482897+lacroixa@users.noreply.github.com>
2022-05-23 18:36:39 +0000

    sw

--- student.py
+++ student.py
@@ -202,15 +202,15 @@ class Piggy(PiggyParent):
         self.fwd()
         self.servo(2000)
         time.sleep(.2)
-        if self.read_distance() < 700:
+        if self.read_distance() < 400:
           self.swerve()
         self.servo(self.MIDPOINT)
         time.sleep(.2)
-        if self.read_distance() < 700:
+        if self.read_distance() < 400:
           self.swerve()
         self.servo(1000)
         time.sleep(.2)
-        if self.read_distance() < 700:
+        if self.read_distance() < 400:
           self.swerve()
 
     def around_wall(self):
